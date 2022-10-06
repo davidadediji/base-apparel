264b3fc44fea4fec3f73611ce6bba60883d0202e
davidadediji <davidadediji@gmail.com>
2022-10-06 14:34:23 +0100

    chore: add error states and hover

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,57 @@
 import React, { useState } from 'react';
 import ArrowRightIcon from '../assets/images/icon-arrow.svg';
-import { Input, Box, Button, Img, Flex } from '@chakra-ui/react';
+import { Input, Box, Button, Img, Flex, Text, color } from '@chakra-ui/react';
+import Error from '../assets/images/icon-error.svg';
 import '../utils/index.css';
 
-
-
 export const Form = () => {
-    const [message, setMessage] = useState('');
-    const [error, setError] = useState('');
-    
+	const [message, setMessage] = useState<string>('');
+	const [isError, setIsError] = useState<Boolean>();
+
+	const handleClick = () => {
+		if (message.includes('@') && message !== '') {
+			setIsError(false);
+		} else {
+			setIsError(true);
+		}
+	};
+
 	return (
-		<Flex
-			border={'1px solid  hsl(0, 36%, 70%)'}
-			borderRadius={'md'}
-			height={'50px'}
-		>
-			<input
-				className='input'
-				type='email'
-				placeholder='Email Address'
-                onChange={(e)=>{
-                    setMessage(e.target.value);
-                    
-                    console.log(e.target.value, 'valid email')
-                }}
-			/>
-			<Button
-				width={'100px'}
-				height={'100%'}
-				borderRadius={'8px'}
-				bg={'linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))'}
+		<>
+			<Flex
+				alignItems={'center'}
+				border={'1px solid  hsl(0, 36%, 70%)'}
+				borderRadius={'md'}
+				height={'50px'}
+                _active={isError && { border: "1px solid hsl(0, 93%, 68%)" }}
 			>
-				<Img src={ArrowRightIcon}></Img>
-			</Button>
-		</Flex>
+				<input
+					className='input'
+					type='email'
+					placeholder='Email Address'
+					onChange={(e) => {
+						setMessage(e.target.value);
+						console.log(message);
+					}}
+				/>
+				{isError && (
+					<Img src={Error} width={'20px'} height={'20px'} mr={'7px'}></Img>
+				)}
+				<Button
+					width={'100px'}
+					height={'100%'}
+					borderRadius={'8px'}
+					bg={'linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))'}
+					onClick={handleClick}
+				>
+					<Img src={ArrowRightIcon}></Img>
+				</Button>
+			</Flex>
+			{isError && (
+				<Text as={'p'} color={'hsl(0, 93%, 68%)'} pl={'10px'} mt={'5px'}>
+					Please provide a valid email.
+				</Text>
+			)}
+		</>
 	);
 };
