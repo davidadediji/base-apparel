268a8babc17dc92c434d1c35fa6050d277b0792c
davidadediji <davidadediji@gmail.com>
2022-10-06 10:55:32 +0100

    chore:add src images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import "@fontsource/josefin-sans"
+
 import { useState } from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, ChakraProvider } from '@chakra-ui/react';
 import BackGround from './assets/images/bg-pattern-desktop.svg';
 import DesktopHeroImage from './assets/images/hero-desktop.jpg';
 import Details from './sections/Details';
 
+import theme from './utils/theme'
+
 function App() {
 	return (
+    <ChakraProvider theme={theme}>
 		<Box as={'body'} bgImage={BackGround} height={'100vh'}>
 			<Flex>
 				<Box width={'60%'} border={'1px solid red'} height={'100vh'}>
@@ -21,6 +26,7 @@ function App() {
 				></Box>
 			</Flex>
 		</Box>
+    </ChakraProvider>
 	);
 }
 
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,14 @@
-import { Box, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
 
 export default function Heading() {
-  return (
-    <Box textTransform={'uppercase'} mt={'12rem'}>
-        <Text as={'h1'}>We're coming soon</Text>
-        
-    </Box>
-  )
+	return (
+		<Box textTransform={'uppercase'} mt={'12rem'}>
+			<Text as={'h1'}>
+				<Text>We're</Text>
+				<Text>coming</Text>
+				<Text>soon</Text>
+			</Text>
+		</Box>
+	);
 }
